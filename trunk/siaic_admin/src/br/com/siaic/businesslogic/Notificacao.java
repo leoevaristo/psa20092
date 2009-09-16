8ba86bcd185979fec66cf786b715257712236ef4
fmacedoo <fmacedoo@4e4e407e-82a1-11de-aa24-d199ae5efac3>
2009-09-16 22:36:07 +0000

    RF_10 Implementação.

--- trunk/siaic_admin/src/br/com/siaic/businesslogic/Notificacao.java
+++ trunk/siaic_admin/src/br/com/siaic/businesslogic/Notificacao.java
@@ -1,5 +1,6 @@
 package br.com.siaic.businesslogic;
 
+import br.com.siaic.dao.DAONotificacao;
 import br.com.siaic.dao.Query;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,31 +11,43 @@ import java.sql.Statement;
 /**
  * 
  * @author Filipe M. de Azevêdo
- *
+ * 
  */
 public class Notificacao {
-	
+
 	/**
-	 * <p> Aciona uma notificação para um imóvel. </p>
-	 * <p> Não obrigatóriamente será agendada e notificada 
-	 * uma visita para um corretor. O método irá buscar
-	 * um cliente com um perfil adequado, caso encontre
-	 * agendará uma visita para um corretor. </p> 
+	 * <p>
+	 * Aciona uma notificação para um imóvel.
+	 * </p>
+	 * <p>
+	 * Não obrigatóriamente será agendada e notificada uma visita para um
+	 * corretor. O método irá buscar um cliente com um perfil adequado, caso
+	 * encontre agendará uma visita para um corretor.
+	 * </p>
+	 * 
 	 * @param codigoImovel
 	 */
-    public void acionar(int codigoImovel) {
-        
-    }
-    
-    private static Notificacao instance;
-    
-    private static Notificacao getInstance() {
-    	if (Notificacao.instance == null)
-    		Notificacao.instance = new Notificacao();
-    	return Notificacao.instance;
-    }
-    
-    private Notificacao() {
-    	
-    }
+	public void acionar(int codigoImovel) {
+		int codigoCaracteristica = this.daoNotificacao.getCodigoCaracteristaImovel(codigoImovel);
+		int codigoPerfil = this.daoNotificacao.getCodigoPerfil(codigoCaracteristica);
+		if (codigoPerfil != -1) {
+			int codigoCliente = this.daoNotificacao.getClientePerfil(codigoPerfil);
+			int codigoCorretor = this.daoNotificacao.getCorretorPerfil(codigoPerfil);
+			this.daoNotificacao.notificar(codigoCliente, codigoCorretor, codigoImovel);
+		}
+	}
+
+	private static Notificacao instance;
+
+	private DAONotificacao daoNotificacao;
+
+	private static Notificacao getInstance() {
+		if (Notificacao.instance == null)
+			Notificacao.instance = new Notificacao();
+		return Notificacao.instance;
+	}
+
+	private Notificacao() {
+		this.daoNotificacao = new DAONotificacao();
+	}
 }
