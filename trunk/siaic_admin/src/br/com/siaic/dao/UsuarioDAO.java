78ab51a8d71acfef0540f6c17064d4db55d66033
arllmcomputacao@gmail.com <arllmcomputacao@gmail.com@4e4e407e-82a1-11de-aa24-d199ae5efac3>
2009-09-17 02:48:15 +0000

    Correção classe UsuarioDAO

--- trunk/siaic_admin/src/br/com/siaic/dao/UsuarioDAO.java
+++ trunk/siaic_admin/src/br/com/siaic/dao/UsuarioDAO.java
@@ -2,9 +2,13 @@ package br.com.siaic.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement; //import java.sql.ResultSet;
+import java.sql.ResultSet;
 import java.sql.SQLException; //import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.List;
 //import java.util.List;
 
+import br.com.siaic.businesslogic.Cliente;
 import br.com.siaic.businesslogic.Usuario;
 
 /**
@@ -13,13 +17,36 @@ import br.com.siaic.businesslogic.Usuario;
  */
 public class UsuarioDAO {
 
+	/**
+	 * 
+	 */
 	private Connection conexao = null;
 
-	public UsuarioDAO() {
+	/**
+	 * 
+	 */
+	private static UsuarioDAO instance = null;
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static UsuarioDAO getInstancia() {
+		if (instance == null) {
+			instance = new UsuarioDAO();
+		}
+
+		return instance;
+	}
+
+	/**
+	 * 
+	 */
+	private UsuarioDAO() {
 
 		try {
 
-			this.conexao = FabricaConexao.conectar();
+			this.conexao = FabricaConexao.getInstancia().conectar();
 
 		}
 
@@ -82,65 +109,48 @@ public class UsuarioDAO {
 
 		catch (Exception e) {
 
-			throw new SQLException("Não foi possível alterar o banco de dados.");
+			throw new SQLException("Nï¿½o foi possï¿½vel alterar o banco de dados.");
 
 		}
 	}
+
+	
+	/**
+	 * 
+	 * @param clienteCodigo
+	 * @return
+	 * @throws SQLException
+	 */
+	public Usuario getUsuarioId(int usuarioCodido) throws SQLException
+	{
+		
+		String sql = "SELECT u.PEU_CODIGO, u.PEU_CRECI, u.PEU_LOGIN, u.PEU_SENHA " 
+					+"FROM PESSOA_USUARIO u, PESSOA p" 
+					+"WHERE u.PEU_CODIGO = ?  AND u.PEU_CODIGO = p.PES_CODIGO ";
+		
+		
+		PreparedStatement ps = conexao.prepareStatement(sql);
+		ps.setInt(1, usuarioCodido);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		Usuario usuario = new Usuario();
+		
+		usuario.setCodigoPessoa(rs.getInt("PEU_CODIGO"));
+		usuario.setCRECI(rs.getString("PEU_CPF"));
+		usuario.setLogin(rs.getString("PEU_RG"));
+		usuario.setSenha(rs.getString("PEU_CNPJ"));
+			
+		ps.close();
+		rs.close();
+		
+		
+		return usuario;
+		
+	}
+	
+
+
+
 }
 
-/*
- * public List < Usuario > getUsuarioPeloNome ( String nome ) throws
- * SQLException {
- * 
- * String sql =
- * "SELECT p.PES_ENDERECO, p.PES_NOME, p.PES_TELEFONE, p.PES_CELULAR, p.PES_EMAIL,"
- * + "c.PEC_CODIGO,c.PEC_CPF,c.PEC_RG,c.PEC_CNPJ" +
- * "e.END_LOGRADOURO,e.END_NOME,e.END_CEP,e.END_BAIRRO,e.END_CODIGO " +
- * "FROM PESSOA p, PESSOA_CLIENTE c, ENDERECO e" +
- * "WHERE p.PES_NOME LIKE ? AND p.PES_ENDERECO = e.END_CODIGO AND p.PES_CODIGO = c.PEC_CODIGO;"
- * ;
- * 
- * 
- * PreparedStatement ps = conexao.prepareStatement(sql); ps.setString(1, nome);
- * 
- * ResultSet rs = ps.executeQuery();
- * 
- * List<Usuario> listaUsuarios = new ArrayList<Usuario>();
- * 
- * while(rs.next()) { Usuario usuario = new Usuario();
- * 
- * usuario.setCodigoPessoa(rs.getInt("PEU_CODIGO"));
- * usuario.setCRECI(rs.getString("PEU_CRECI"));
- * usuario.setLogin(rs.getString("PEU_LOGIN"));
- * usuario.setSenha(rs.getString("PEU_SENHA"));
- * 
- * listaUsuarios.add(usuario);
- * 
- * }
- * 
- * ps.close(); rs.close();
- * 
- * 
- * 
- * 
- * return listaUsuarios;
- * 
- * }
- * 
- * 
- * 
- * 
- * public List<Usuario> getTodosUsuarios () { //TODO return new
- * ArrayList<Usuario>(); }
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * }
- */
