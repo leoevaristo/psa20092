c300aeb07b7be8f685943ad3c6079b15c9859662
robinhocunha <robinhocunha@4e4e407e-82a1-11de-aa24-d199ae5efac3>
2009-09-17 14:55:40 +0000

    Novos métodos adicionados para maiores detalhes olhe no comparador de versões

--- trunk/siaic_admin/src/br/com/siaic/dao/AgendaDAO.java
+++ trunk/siaic_admin/src/br/com/siaic/dao/AgendaDAO.java
@@ -7,10 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.siaic.businesslogic.Agenda;
+import br.com.siaic.businesslogic.Cliente;
+import br.com.siaic.businesslogic.Usuario;
 
 /**
- * Singleton - Responsavel por todas as consultas, inseções, exclusões, edições e trasações
- * com a base de dados na tabela "dbo.siaic.agenda".
+ * Singleton - Responsavel por todas as consultas, inseções, exclusões, edições
+ * e trasações com a base de dados na tabela "dbo.siaic.agenda".
  * 
  * @author Robson R. Vieira da Cunha
  * @version 1.0
@@ -41,8 +43,7 @@ public class AgendaDAO {
 	 */
 	public Agenda getAgenda(int codigo) throws SQLException {
 		String query = new String("select * from agenda where AGE_CODIGO = ?");
-		PreparedStatement ps;
-		ps = DB.getConn().prepareStatement(query);
+		PreparedStatement ps = DB.getConn().prepareStatement(query);
 		ps.setInt(1, codigo);
 		ResultSet rs = ps.executeQuery();
 
@@ -54,8 +55,8 @@ public class AgendaDAO {
 					rs.getInt("AGE_PESSOA_CLIENTE")));
 			a.setCorretor(UsuarioDAO.getInstancia().getUsuarioId(
 					rs.getInt("AGE_PESSOA_USUARIO")));
-			//a.setImovel(ImovelDAO.getInstance().getImovel(
-			//		rs.getInt("AGE_PESSOA_USUARIO")));
+			// a.setImovel(ImovelDAO.getInstance().getImovel(
+			// rs.getInt("AGE_PESSOA_USUARIO")));
 			a.setData(rs.getDate("AGE_DATA"));
 			a.setHoraInicio(rs.getTime("AGE_HORA_INICIO"));
 			a.setHoraFim(rs.getTime("AGE_HORA_INICIO"));
@@ -65,16 +66,21 @@ public class AgendaDAO {
 		ps.close();
 		return a;
 	}
-	
-	public List<Agenda> getAgendaList() throws SQLException{
+
+	/**
+	 * Lista todas as ocorrencias da tabela Agenda
+	 * 
+	 * @return Lista de {@link Agenda}
+	 * @throws SQLException
+	 */
+	public List<Agenda> getAgendaList() throws SQLException {
 		String query = new String("select * from agenda");
-		PreparedStatement ps;
-		ps = DB.getConn().prepareStatement(query);
+		PreparedStatement ps = DB.getConn().prepareStatement(query);
 		ResultSet rs = ps.executeQuery();
 
 		List<Agenda> l = new ArrayList<Agenda>();
 		Agenda a = null;
-		
+
 		while (rs.next()) {
 			a = new Agenda();
 			a.setCodigo(rs.getInt("AGE_CODIGO"));
@@ -82,8 +88,8 @@ public class AgendaDAO {
 					rs.getInt("AGE_PESSOA_CLIENTE")));
 			a.setCorretor(UsuarioDAO.getInstancia().getUsuarioId(
 					rs.getInt("AGE_PESSOA_USUARIO")));
-			//a.setImovel(ImovelDAO.getInstance().getImovel(
-			//		rs.getInt("AGE_PESSOA_USUARIO")));
+			// a.setImovel(ImovelDAO.getInstance().getImovel(
+			// rs.getInt("AGE_PESSOA_USUARIO")));
 			a.setData(rs.getDate("AGE_DATA"));
 			a.setHoraInicio(rs.getTime("AGE_HORA_INICIO"));
 			a.setHoraFim(rs.getTime("AGE_HORA_INICIO"));
@@ -93,5 +99,126 @@ public class AgendaDAO {
 		rs.close();
 		ps.close();
 		return l;
+	}
+
+	/**
+	 * Lista todas as ocorrencias da tabela Agenda de um determinado Usuário
+	 * (Corretor)
+	 * 
+	 * @param u
+	 *            {@link Usuario} (Corretor)
+	 * @return Lista de {@link Agenda}
+	 * @throws SQLException
+	 */
+	public List<Agenda> getAgendaList(Usuario u) throws SQLException {
+		String query = new String(
+				"select * from agenda where AGE_PESSOA_USUARIO = ?");
+		PreparedStatement ps = DB.getConn().prepareStatement(query);
+		ps.setInt(1, u.getCodigoPessoa());
+		ResultSet rs = ps.executeQuery();
+
+		List<Agenda> l = new ArrayList<Agenda>();
+		Agenda a = null;
+
+		while (rs.next()) {
+			a = new Agenda();
+			a.setCodigo(rs.getInt("AGE_CODIGO"));
+			a.setCliente(ClienteDAO.getInstancia().getClientePorId(
+					rs.getInt("AGE_PESSOA_CLIENTE")));
+			a.setCorretor(UsuarioDAO.getInstancia().getUsuarioId(
+					rs.getInt("AGE_PESSOA_USUARIO")));
+			// a.setImovel(ImovelDAO.getInstance().getImovel(
+			// rs.getInt("AGE_PESSOA_USUARIO")));
+			a.setData(rs.getDate("AGE_DATA"));
+			a.setHoraInicio(rs.getTime("AGE_HORA_INICIO"));
+			a.setHoraFim(rs.getTime("AGE_HORA_INICIO"));
+			// a.setDescricao(rs.getString("AGE_DESCRICAO"));
+			l.add(a);
+		}
+		rs.close();
+		ps.close();
+		return l;
+	}
+
+	/**
+	 * Lista todas as ocorrencias da tabela Agenda de um determinado Cliente
+	 * 
+	 * @param c
+	 *            {@link Cliente}
+	 * @return Lista de {@link Agenda}
+	 * @throws SQLException
+	 */
+	public List<Agenda> getAgendaList(Cliente c) throws SQLException {
+		String query = new String(
+				"select * from agenda where AGE_PESSOA_CLIENTE = ?");
+		PreparedStatement ps = DB.getConn().prepareStatement(query);
+		ps.setInt(1, c.getCodigoPessoa());
+		ResultSet rs = ps.executeQuery();
+
+		List<Agenda> l = new ArrayList<Agenda>();
+		Agenda a = null;
+
+		while (rs.next()) {
+			a = new Agenda();
+			a.setCodigo(rs.getInt("AGE_CODIGO"));
+			a.setCliente(ClienteDAO.getInstancia().getClientePorId(
+					rs.getInt("AGE_PESSOA_CLIENTE")));
+			a.setCorretor(UsuarioDAO.getInstancia().getUsuarioId(
+					rs.getInt("AGE_PESSOA_USUARIO")));
+			// a.setImovel(ImovelDAO.getInstance().getImovel(
+			// rs.getInt("AGE_PESSOA_USUARIO")));
+			a.setData(rs.getDate("AGE_DATA"));
+			a.setHoraInicio(rs.getTime("AGE_HORA_INICIO"));
+			a.setHoraFim(rs.getTime("AGE_HORA_INICIO"));
+			// a.setDescricao(rs.getString("AGE_DESCRICAO"));
+			l.add(a);
+		}
+		rs.close();
+		ps.close();
+		return l;
+	}
+
+	/**
+	 * Lista todas as ocorrencias da tabela Agenda de um determinado Imóvel
+	 * 
+	 * @param i
+	 *            {@link Imovel}
+	 * @return Lista de {@link Agenda}
+	 * @throws SQLException
+	 */
+	/*public List<Agenda> getAgendaList(Imovel i) throws SQLException {
+		String query = new String(
+				"select * from agenda where AGE_IMOVEL = ?");
+		PreparedStatement ps = DB.getConn().prepareStatement(query);
+		ps.setInt(1, i.getCodigo());
+		ResultSet rs = ps.executeQuery();
+
+		List<Agenda> l = new ArrayList<Agenda>();
+		Agenda a = null;
+
+		while (rs.next()) {
+			a = new Agenda();
+			a.setCodigo(rs.getInt("AGE_CODIGO"));
+			a.setCliente(ClienteDAO.getInstancia().getClientePorId(
+					rs.getInt("AGE_PESSOA_CLIENTE")));
+			a.setCorretor(UsuarioDAO.getInstancia().getUsuarioId(
+					rs.getInt("AGE_PESSOA_USUARIO")));
+			// a.setImovel(ImovelDAO.getInstance().getImovel(
+			// rs.getInt("AGE_PESSOA_USUARIO")));
+			a.setData(rs.getDate("AGE_DATA"));
+			a.setHoraInicio(rs.getTime("AGE_HORA_INICIO"));
+			a.setHoraFim(rs.getTime("AGE_HORA_INICIO"));
+			// a.setDescricao(rs.getString("AGE_DESCRICAO"));
+			l.add(a);
+		}
+		rs.close();
+		ps.close();
+		return l;
+	}*/
+	
+	public boolean AtualizarAgenda(Agenda atual, Agenda nova){
+		
+		return false;
+		
 	}
 }
