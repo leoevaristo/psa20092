14418ad859be6e4f4dc90effd4a179e2be73adb9
carlos.cstds <carlos.cstds@4e4e407e-82a1-11de-aa24-d199ae5efac3>
2009-09-17 21:22:41 +0000

    Fixando conflitos criados por um commit errôneo.

---
+++ siaic_admin/src/br/com/siaic/dao/PessoaDAO.java
@@ -0,0 +1,111 @@
+package br.com.siaic.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.siaic.businesslogic.Pessoa;
+
+public class PessoaDAO 
+{
+	
+	
+	
+	
+	private Connection conexao = null;
+	
+	
+	
+	
+	private static PessoaDAO instancia = null;
+	
+	
+	
+	
+	private PessoaDAO()
+	{
+		
+		try
+		{
+			FabricaConexao.getInstancia();
+			this.conexao = FabricaConexao.conectar();
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	public static PessoaDAO getInstancia()
+	{
+		
+		if(instancia == null)
+		{
+			instancia = new PessoaDAO();
+		}
+		
+		return instancia;
+		
+	}
+	
+	
+	
+	
+	public void adicionarPessoa(Pessoa pessoa) throws SQLException
+	{
+		
+		String sql = "INSERT INTO PESSOA (PES_NOME, PES_TELEFONE, PES_CELULAR, " 
+			       + "PES_EMAIL, PES_TIPO, PES_SEXO, PES_ENDERECO) " 
+			       + "VALUES (? , ? , ? , ? , ? , ? , ? );";
+		
+		
+		PreparedStatement ps = conexao.prepareStatement(sql);
+		
+		ps.setString(1, pessoa.getNome());
+		ps.setString(2, pessoa.getTelefone());
+		ps.setString(3, pessoa.getCelular());
+		ps.setString(4, pessoa.getEmail());
+		ps.setString(5, pessoa.getTipoPessoa());
+		ps.setString(6, pessoa.getSexo());
+		ps.setInt(7, 1);
+		
+		ps.execute();
+		
+		setIdPessoa(pessoa);		
+		
+		ps.close();
+		
+	}
+	
+	
+	
+	
+	
+	public void setIdPessoa(Pessoa pessoa) throws SQLException
+	{
+		
+		String sql = "SELECT MAX(PES_CODIGO) AS CODIGO FROM PESSOA;";
+		
+		PreparedStatement ps = conexao.prepareStatement(sql);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next())
+		{
+			
+			pessoa.setCodigoPessoa(rs.getInt("CODIGO"));
+		
+		}				
+		
+		ps.close();
+		
+		rs.close();
+	}
+	
+
+}
