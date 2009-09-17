448978436b4cd6768b759d250870fcc2f10fb33c
robinhocunha <robinhocunha@4e4e407e-82a1-11de-aa24-d199ae5efac3>
2009-09-17 17:05:21 +0000

    Novos métodos adicionados. Maiores detalhes favor olhar no comparador de versões.

--- siaic_admin/src/br/com/siaic/dao/AgendaDAO.java
+++ siaic_admin/src/br/com/siaic/dao/AgendaDAO.java
@@ -184,41 +184,91 @@ public class AgendaDAO {
 	 * @param i
 	 *            {@link Imovel}
 	 * @return Lista de {@link Agenda}
+	 * @throws SQLException 
 	 * @throws SQLException
 	 */
-	/*public List<Agenda> getAgendaList(Imovel i) throws SQLException {
-		String query = new String(
-				"select * from agenda where AGE_IMOVEL = ?");
+//	public List<Agenda> getAgendaList(Imovel i) throws SQLException {
+//		String query = new String(
+//				"select * from agenda where AGE_IMOVEL = ?");
+//		PreparedStatement ps = DB.getConn().prepareStatement(query);
+//		ps.setInt(1, i.getCodigo());
+//		ResultSet rs = ps.executeQuery();
+//
+//		List<Agenda> l = new ArrayList<Agenda>();
+//		Agenda a = null;
+//
+//		while (rs.next()) {
+//			a = new Agenda();
+//			a.setCodigo(rs.getInt("AGE_CODIGO"));
+//			a.setCliente(ClienteDAO.getInstancia().getClientePorId(
+//					rs.getInt("AGE_PESSOA_CLIENTE")));
+//			a.setCorretor(UsuarioDAO.getInstancia().getUsuarioId(
+//					rs.getInt("AGE_PESSOA_USUARIO")));
+//			// a.setImovel(ImovelDAO.getInstance().getImovel(
+//			// rs.getInt("AGE_PESSOA_USUARIO")));
+//			a.setData(rs.getDate("AGE_DATA"));
+//			a.setHoraInicio(rs.getTime("AGE_HORA_INICIO"));
+//			a.setHoraFim(rs.getTime("AGE_HORA_INICIO"));
+//			// a.setDescricao(rs.getString("AGE_DESCRICAO"));
+//			l.add(a);
+//		}
+//		rs.close();
+//		ps.close();
+//		return l;
+//	}
+
+	/**
+	 * Atualiza um registro na tabela Agenda.</br>Substitui o registro "atual"
+	 * representado pelo objeto passado por parâmetro pelo objeto "novo" também
+	 * passado por parâmetro.
+	 * 
+	 * @param atual
+	 *            - {@link Agenda} representa o registro atual na tabela
+	 *            "Agenda"
+	 * @param nova
+	 *            - {@link Agenda} representa o registro novo que substituirá o
+	 *            atual.
+	 * @return <code>true</code> caso a atualização seja realizada com sucesso
+	 *         ou <code>false</code> caso contrário
+	 * @throws SQLException
+	 */
+	public boolean AtualizarAgenda(Agenda atual, Agenda nova) throws SQLException {
+		String query = new String("update agenda set "
+				+ "AGE_CODIGO = ?, AGE_PESSOA_CLIENTE = ?, "
+				+ "AGE_PESSOA_USUARIO = ?, AGE_IMOVEL = ?, "
+				+ "AGE_DATA = ?, AGE_HORA_INICIO = ?, "
+				+ "AGE_HORA_FIM = ?, AGE_DESCRICAO = ? "
+				+ "where AGE_CODIGO = ?");
 		PreparedStatement ps = DB.getConn().prepareStatement(query);
-		ps.setInt(1, i.getCodigo());
-		ResultSet rs = ps.executeQuery();
-
-		List<Agenda> l = new ArrayList<Agenda>();
-		Agenda a = null;
-
-		while (rs.next()) {
-			a = new Agenda();
-			a.setCodigo(rs.getInt("AGE_CODIGO"));
-			a.setCliente(ClienteDAO.getInstancia().getClientePorId(
-					rs.getInt("AGE_PESSOA_CLIENTE")));
-			a.setCorretor(UsuarioDAO.getInstancia().getUsuarioId(
-					rs.getInt("AGE_PESSOA_USUARIO")));
-			// a.setImovel(ImovelDAO.getInstance().getImovel(
-			// rs.getInt("AGE_PESSOA_USUARIO")));
-			a.setData(rs.getDate("AGE_DATA"));
-			a.setHoraInicio(rs.getTime("AGE_HORA_INICIO"));
-			a.setHoraFim(rs.getTime("AGE_HORA_INICIO"));
-			// a.setDescricao(rs.getString("AGE_DESCRICAO"));
-			l.add(a);
-		}
-		rs.close();
+		ps.setInt(1, nova.getCodigo());
+		ps.setInt(2, nova.getCliente().getCodigoPessoa());
+		ps.setInt(3, nova.getCorretor().getCodigoPessoa());
+		//ps.setInt(4, nova.getImovel().getCodigoImovel());
+		ps.setDate(5, nova.getData());
+		ps.setTime(6, nova.getHoraInicio());
+		ps.setTime(7, nova.getHoraFim());
+		ps.setString(8, nova.getDescricao());
+		ps.setInt(9, atual.getCodigo());
+		
+		boolean result = ps.executeUpdate() > 0;
 		ps.close();
-		return l;
-	}*/
+		return result;
+	}
 	
-	public boolean AtualizarAgenda(Agenda atual, Agenda nova){
+	/**
+	 * 
+	 * @param a
+	 * @return <code>true</code> caso a atualização seja realizada com sucesso
+	 *         ou <code>false</code> caso contrário
+	 * @throws SQLException
+	 */
+	public boolean ApagarAgenda(Agenda a) throws SQLException{
+		String query = new String("delete from agenda where AGE_CODIGO = ?");
+		PreparedStatement ps = DB.getConn().prepareStatement(query);
+		ps.setInt(1, a.getCodigo());
 		
-		return false;
-		
+		boolean result = ps.executeUpdate() > 0;
+		ps.close();
+		return result;
 	}
 }
