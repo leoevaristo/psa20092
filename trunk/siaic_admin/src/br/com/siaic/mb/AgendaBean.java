323a0dde256760ff83347a598b4d74f0e967b549
robinhocunha <robinhocunha@4e4e407e-82a1-11de-aa24-d199ae5efac3>
2009-09-18 14:31:32 +0000

    AgendaBean Finalizado

--- trunk/siaic_admin/src/br/com/siaic/mb/AgendaBean.java
+++ trunk/siaic_admin/src/br/com/siaic/mb/AgendaBean.java
@@ -1,15 +1,32 @@
 package br.com.siaic.mb;
 
+import java.sql.SQLException;
+
 import br.com.siaic.businesslogic.Agenda;
+import br.com.siaic.dao.AgendaDAO;
 
 public class AgendaBean {
-	
-	private Agenda agenda;
-	
-	public AgendaBean() {
-		agenda = new Agenda();
-	}
-	
-	public AgendaBean
 
+	private Agenda agenda;
+	private int codigoAgenda;
+
+	public AgendaBean() {
+		this.agenda = new Agenda();
+	}
+
+	public Agenda getAgenda() {
+		return this.agenda;
+	}
+
+	public int getCodigoAgenda() {
+		return this.codigoAgenda;
+	}
+
+	public void setCodigoAgenda(int codigoAgenda) {
+		this.codigoAgenda = codigoAgenda;
+	}
+
+	public void consultaAgenda() throws SQLException {
+		this.agenda = AgendaDAO.getInstance().getAgenda((this.codigoAgenda));
+	}
 }
