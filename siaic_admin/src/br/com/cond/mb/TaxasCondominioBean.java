3214da038578e16267318e4c4b9446f2e2744cfa
carlos.cstds <carlos.cstds@4e4e407e-82a1-11de-aa24-d199ae5efac3>
2009-10-15 11:06:43 +0000

    # Implementação do método que adiciona taxas de condomínio a todos apartamentos  - RF_Cadastro e Manutenção de Taxas de Condomínio # Alterações e criação de novos templates, para separar as duas aplicações.

--- siaic_admin/src/br/com/cond/mb/TaxasCondominioBean.java
+++ siaic_admin/src/br/com/cond/mb/TaxasCondominioBean.java
@@ -1,7 +1,15 @@
 package br.com.cond.mb;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 import br.com.cond.businesslogic.Apartamento;
 import br.com.cond.businesslogic.TaxasCondominio;
+import br.com.cond.dao.ApartamentoDAO;
+import br.com.cond.dao.TaxasCondominioDAO;
 
 public class TaxasCondominioBean {
 	
@@ -9,8 +17,50 @@ public class TaxasCondominioBean {
 	
 	private TaxasCondominio taxasCondominio;
 	
+	public TaxasCondominioBean(){
+		apartamento = new Apartamento();
+		taxasCondominio = new TaxasCondominio();
+	}
 	
-	public String cadastrar(){
+	
+	public Apartamento getApartamento() {
+		return apartamento;
+	}
+
+
+	public void setApartamento(Apartamento apartamento) {
+		this.apartamento = apartamento;
+	}
+
+
+	public TaxasCondominio getTaxasCondominio() {
+		return taxasCondominio;
+	}
+
+
+	public void setTaxasCondominio(TaxasCondominio taxasCondominio) {
+		this.taxasCondominio = taxasCondominio;
+	}
+
+
+	public String cadastrar() throws SQLException{
+		ApartamentoDAO daoApartamento = new ApartamentoDAO();
+		TaxasCondominioDAO daoTaxas = new TaxasCondominioDAO();
+		List<Apartamento> todosApartamentos = daoApartamento.getTodosOsApartamentos();
+		
+		for(Apartamento ap : todosApartamentos){
+			getTaxasCondominio().setApartamento(ap);
+			if(daoTaxas.addTaxaCondominio(this.taxasCondominio) == false){
+				FacesContext ctx = FacesContext.getCurrentInstance();
+				FacesMessage msg = new FacesMessage("As taxas para esse mês já foram geradas.");
+				ctx.addMessage("formTaxa", msg);
+			}else{
+				FacesContext ctx = FacesContext.getCurrentInstance();
+				FacesMessage msg = new FacesMessage("Taxas cadastradas com sucesso.");
+				ctx.addMessage("formTaxa", msg);				
+			}
+		}
+		
 		return null;
 		
 	}
@@ -24,6 +74,8 @@ public class TaxasCondominioBean {
 		return null;
 		
 	}
+
+
 	
 
 }
